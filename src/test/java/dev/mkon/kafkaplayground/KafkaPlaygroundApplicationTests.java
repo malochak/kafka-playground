4ad19f38388526cd527382fe78f9621ae9d3017d
malochak <konrad_malocha@hotmail.com>
2023-10-13 21:46:42 +0200

    bootstrap kafka playground project

---
+++ src/test/java/dev/mkon/kafkaplayground/KafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mkon.kafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
