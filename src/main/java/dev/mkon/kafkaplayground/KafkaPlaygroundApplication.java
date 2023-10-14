727f5fbd8fc4a55bf978cd6bf36a0b6d00b47852
malochak <konrad_malocha@hotmail.com>
2023-10-14 11:18:24 +0200

    implement sending simple message to example-topic

--- src/main/java/dev/mkon/kafkaplayground/KafkaPlaygroundApplication.java
+++ src/main/java/dev/mkon/kafkaplayground/KafkaPlaygroundApplication.java
@@ -1,7 +1,10 @@
 package dev.mkon.kafkaplayground;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaPlaygroundApplication {
@@ -10,4 +13,12 @@ public class KafkaPlaygroundApplication {
         SpringApplication.run(KafkaPlaygroundApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+        return args -> {
+            for (int i = 0; i < 100_000; i++) {
+                kafkaTemplate.send("example-topic", "%d Hello Hello KAFKA!".formatted(i));
+            }
+        };
+    }
 }
--- src/main/java/dev/mkon/kafkaplayground/config/KafkaTopicConfig.java
+++ src/main/java/dev/mkon/kafkaplayground/config/KafkaTopicConfig.java
@@ -10,7 +10,7 @@ public class KafkaTopicConfig {
 
     @Bean
     public NewTopic exampleTopic() {
-        return TopicBuilder.name("exampletopic")
+        return TopicBuilder.name("example-topic")
             .build();
     }
 }
