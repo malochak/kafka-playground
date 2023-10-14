a994a8d9939b6ac7fb5c369939c1871e432d2dd4
malochak <konrad_malocha@hotmail.com>
2023-10-14 10:54:04 +0200

    add kafka basic cfg

---
+++ src/main/java/dev/mkon/kafkaplayground/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package dev.mkon.kafkaplayground.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic exampleTopic() {
+        return TopicBuilder.name("exampletopic")
+            .build();
+    }
+}
